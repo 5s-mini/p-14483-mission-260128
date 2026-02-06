64f97ea026acbebbd8b85ef9c55a579811448523
5s-mini <dnclsehd123@naver.com>
2026-02-06 16:36:19 +0900

    feat: 사칙연산 기능 추가, 연산 순서 및 괄호 확인 미구현

--- src/main/java/tddcalc/Calc.java
+++ src/main/java/tddcalc/Calc.java
@@ -2,6 +2,21 @@ package tddcalc;
 
 public class Calc {
     public static int run(String input) {
+        String[] tokens = input.split(" ");
+        int result = Integer.parseInt(tokens[0]);
 
+        for (int i = 1; i < tokens.length; i += 2) {
+            String operator = tokens[i];
+            int number = Integer.parseInt(tokens[i + 1]);
+
+            switch (operator) {
+                case "+" -> result += number;
+                case "-" -> result -= number;
+                case "*" -> result *= number;
+                case "/" -> result /= number;
+            }
+        }
+
+        return result;
     }
 }
